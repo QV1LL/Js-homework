a67930d0f6b8ea1a5283f82f314137d1e7934dbb
QV1LL <ghost228678@gmail.com>
2026-02-11 23:27:10 +0200

    Fix hm25

--- Homework25 (react-router)/src/main.tsx
+++ Homework25 (react-router)/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/providers/router'
+import { router } from './app'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
